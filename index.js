e872480eacfee2db46831eb33c17a5be63a3cb2a
Taemin Kim <taeminkimny@gmail.com>
2022-05-16 17:24:12 -0400

    commit tree e7d5078ed6240a2d941a4ca50f2dfaf6b4286de9

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+
+let distance = 0;
+
+// calculate distance from hq to pick up location in blocks
+function distanceFromHqInBlocks(pickupLocation) {
+    let distance = Math.abs(pickupLocation - 42);
+    return distance;
+}
+
+// calculate the distance in feet
+function distanceFromHqInFeet(pickupLocation) {
+    const numOfBlocks = distanceFromHqInBlocks(pickupLocation);
+    const distanceInFeet = numOfBlocks * 264;
+    return distanceInFeet;
+}
+
+
+function distanceTravelledInFeet(start, destination) {
+    const travelDistance = Math.abs(start - destination) * 264;
+    return travelDistance;
+
+}
+
+function calculatesFarePrice(start, destination) {
+    const travelDistance = distanceTravelledInFeet(start, destination);
+    return (travelDistance <= 400) ? 0
+        :  (travelDistance >= 400 && travelDistance <= 2000) ? Math.abs(travelDistance-400) * .02
+        :  (travelDistance >= 2000 && travelDistance <= 2500) ? 25
+        :  'cannot travel that far';
+}
